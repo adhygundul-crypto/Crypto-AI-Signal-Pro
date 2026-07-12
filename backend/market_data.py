870ac54e7a4d38987cf52327114f22d85aa1f7ee
adhygundul-crypto <adhygundul@gmail.com>
2026-07-12 22:59:21 +0800

    Update market_data.py

--- backend/market_data.py
+++ backend/market_data.py
@@ -1,1 +1,17 @@
+from binance.client import Client
 
+client = Client()
+
+
+def get_price(symbol="BTCUSDT"):
+    ticker = client.get_symbol_ticker(symbol=symbol)
+
+    return {
+        "symbol": symbol,
+        "price": float(ticker["price"])
+    }
+
+
+if __name__ == "__main__":
+    data = get_price()
+    print(data)
