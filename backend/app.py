05de9169ce133abdd9df44f5a0652632e1d870e0
adhygundul-crypto <adhygundul@gmail.com>
2026-07-12 23:01:12 +0800

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -1,1 +1,19 @@
+from fastapi import FastAPI
 
+from market_data import get_price
+
+app = FastAPI(
+    title="Crypto AI Signal Pro"
+)
+
+
+@app.get("/")
+def home():
+    return {
+        "message": "Crypto AI Signal Pro API"
+    }
+
+
+@app.get("/price")
+def price():
+    return get_price()
